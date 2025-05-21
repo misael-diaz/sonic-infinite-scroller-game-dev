2c1f1403b91c8ee9b662684db04cf1d83033b1e4
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-05-21 18:15:07 -0400

    renders background in X11 Window
    NOTES
    a lot of the xlib related code was borrowed from id's Quake-III
    
    we don't need the colormap generated by libpng consider removing that
    
    we have to wait for the window exposure event before displaying anything
    
    we read from stdin as a means to implement a pause in execution
    
    destroying the image also frees the data so no need to free the
    data manually
    
    we have confirmed that there are no memory related issues with
    valgrind
    
    we write the PNG in the /tmp directory to check that indeed we
    were able to read the image file properly
    
    we had to set the RGBA format according to the visual settings,
    we checked that blue, green, and red are expected in that order
    
    took me a lot of time and effor to get this right but it was all
    worth it

--- src/main.c
+++ src/main.c
@@ -23,51 +23,204 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <string.h>
 #include <errno.h>
 #include <png.h>
 
-void *load (char const * const fileNamePNG)
+void load(
+		void **cmp,
+		void **png,
+		png_image * const imp,
+		char const * const fileNamePNG
+)
 {
-	png_image img = {
-		.version = PNG_IMAGE_VERSION,
-		.format = PNG_FORMAT_RGBA
-	};
+	void *colormap = NULL;
+	imp->version = PNG_IMAGE_VERSION;
 	png_byte *bufp = NULL;
-	if (png_image_begin_read_from_file(&img, fileNamePNG)) {
-		img.format = PNG_FORMAT_RGBA;
-		bufp = malloc(PNG_IMAGE_SIZE(img));
+	if (png_image_begin_read_from_file(imp, fileNamePNG)) {
+		imp->format = PNG_FORMAT_BGRA;
+		bufp = malloc(PNG_IMAGE_SIZE(*imp));
 		if (!bufp) {
 			fprintf(stderr, "load: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		}
-		if (!png_image_finish_read(&img, NULL, bufp, 0, NULL)) {
-			fprintf(stderr, "load: %s\n", img.message);
-			png_image_free(&img);
+		colormap = malloc(PNG_IMAGE_COLORMAP_SIZE(*imp));
+		memset(colormap, 0, PNG_IMAGE_COLORMAP_SIZE(*imp));
+		if (!colormap) {
 			free(bufp);
+			fprintf(stderr, "load: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		if (!png_image_finish_read(imp, NULL, bufp, 0, colormap)) {
+			fprintf(stderr, "load: %s\n", imp->message);
+			free(bufp);
+			free(colormap);
+			png_image_free(imp);
+			exit(EXIT_FAILURE);
+		}
+		if (!png_image_write_to_file(imp, "/tmp/tmp.png", 0, bufp, 0, colormap)) {
+			fprintf(stderr, "load: %s\n", imp->message);
+			free(bufp);
+			free(colormap);
+			png_image_free(imp);
 			exit(EXIT_FAILURE);
 		}
 	}
 	fprintf(stdout, "load: loaded-png: %s\n", fileNamePNG);
-	return bufp;
+	*cmp = colormap;
+	*png = bufp;
 }
 
 int main(void)
 {
-	char const * const background = "../public/graphics/chemical-bg.png";
-	char const * const bg = background;
-	void *png = load(bg);
+	//char const * const pngfile = "public/graphics/sonic.png";
+	char const * const pngfile = "public/graphics/bg.png";
+	png_image img = {};
+	void *png = NULL;
+	void *map = NULL;
+	load(&map, &png, &img, pngfile);
+	size_t const bytes_map = PNG_IMAGE_COLORMAP_SIZE(img);
+	size_t const bytes_png = PNG_IMAGE_SIZE(img);
 	Display *display = XOpenDisplay(NULL);
 	if (!display) {
 		fprintf(stderr, "%s\n", "main: XOpenDisplayError");
+		free(map);
 		free(png);
+		map = NULL;
+		png = NULL;
 		exit(EXIT_FAILURE);
 	}
+
 	int screen = XDefaultScreen(display);
 	fprintf(stdout, "main: using screen %d\n", screen);
+	Visual *visual = XDefaultVisual(display, screen);
+	if (!visual) {
+		fprintf(stderr, "%s\n", "main: XDefaultVisualError");
+		free(map);
+		free(png);
+		XCloseDisplay(display);
+		display = NULL;
+		map = NULL;
+		png = NULL;
+		exit(EXIT_FAILURE);
+	}
+
+	int numVisuals = 0;
+	VisualID visualID = XVisualIDFromVisual(visual);
+	XVisualInfo tmpXVisualInfo = {
+		.visualid = visualID
+	};
+	XVisualInfo *visualInfo = XGetVisualInfo(
+			display,
+			VisualIDMask,
+			&tmpXVisualInfo,
+			&numVisuals
+	);
+	if (!visualInfo) {
+		fprintf(stderr, "%s\n", "main: XGetVisualInfoError");
+		free(map);
+		free(png);
+		XFree(visualInfo);
+		XCloseDisplay(display);
+		display = NULL;
+		map = NULL;
+		png = NULL;
+		exit(EXIT_FAILURE);
+	}
+
+	int const windowXPos = 0;
+	int const windowYPos = 0;
+	int const windowWidth = img.width;
+	int const windowHeight = img.height;
+	int const windowBorderWidth = 0;
+	unsigned long const windowBorder = 0;
+	unsigned long const windowBackground = 0;
+	Window window = XCreateSimpleWindow(
+			display,
+			XRootWindow(display, screen),
+			windowXPos,
+			windowYPos,
+			windowWidth,
+			windowHeight,
+			windowBorderWidth,
+			windowBorder,
+			windowBackground
+	);
+	XStoreName(display, window, "SonicInfiniteScroller");
+
+	int const graphicsContextFlags = GCGraphicsExposures;
+	XGCValues graphicsContextValues = {
+		.graphics_exposures = 1,
+	};
+	GC gc = XCreateGC(
+			display,
+			window,
+			graphicsContextFlags,
+			&graphicsContextValues
+	);
+
+	XSelectInput(display, window, ExposureMask);
+	XMapWindow(display, window);
+
+	XEvent ev = {};
+	XWindowEvent(display, window, ExposureMask, &ev);
+
+	int const offset = 0;
+	int const bitmap_pad = 32;
+	int const bytes_per_line = (img.width * sizeof(int));
+	XImage *framebuffer = XCreateImage(
+			display,
+			visual,
+			visualInfo->depth,
+			ZPixmap,
+			offset,
+			png,
+			img.width,
+			img.height,
+			bitmap_pad,
+			bytes_per_line
+	);
+
+	if (!framebuffer) {
+		fprintf(stderr, "%s\n", "main: XCreateImageError");
+		free(map);
+		free(png);
+		XFree(visualInfo);
+		XCloseDisplay(display);
+		display = NULL;
+		map = NULL;
+		png = NULL;
+		exit(EXIT_FAILURE);
+	}
+
+	XPutImage(
+			display,
+			window,
+			gc,
+			framebuffer,
+			0,
+			0,
+			0,
+			0,
+			img.width,
+			img.height
+	);
+
+	char c;
+	fprintf(stdout, "%s\n", "main: press any key to continue");
+	fread(&c, sizeof(c), 1, stdin);
+
+	XDestroyImage(framebuffer);
+	XFreeGC(display, gc);
+	XFree(visualInfo);
+	XDestroyWindow(display, window);
 	XCloseDisplay(display);
-	free(png);
+	free(map);
+	visual = NULL;
+	framebuffer = NULL;
+	visualInfo = NULL;
 	display = NULL;
-	png = NULL;
+	map = NULL;
 	return 0;
 }
