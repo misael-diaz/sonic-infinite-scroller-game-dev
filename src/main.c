527d445c0d02ff785dadfac084885a7e428271f8
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-05-21 14:45:01 -0400

    loads background PNG image

--- src/main.c
+++ src/main.c
@@ -23,17 +23,51 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/Xlib.h>
+#include <string.h>
+#include <errno.h>
+#include <png.h>
+
+void *load (char const * const fileNamePNG)
+{
+	png_image img = {
+		.version = PNG_IMAGE_VERSION,
+		.format = PNG_FORMAT_RGBA
+	};
+	png_byte *bufp = NULL;
+	if (png_image_begin_read_from_file(&img, fileNamePNG)) {
+		img.format = PNG_FORMAT_RGBA;
+		bufp = malloc(PNG_IMAGE_SIZE(img));
+		if (!bufp) {
+			fprintf(stderr, "load: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		if (!png_image_finish_read(&img, NULL, bufp, 0, NULL)) {
+			fprintf(stderr, "load: %s\n", img.message);
+			png_image_free(&img);
+			free(bufp);
+			exit(EXIT_FAILURE);
+		}
+	}
+	fprintf(stdout, "load: loaded-png: %s\n", fileNamePNG);
+	return bufp;
+}
 
 int main(void)
 {
+	char const * const background = "../public/graphics/chemical-bg.png";
+	char const * const bg = background;
+	void *png = load(bg);
 	Display *display = XOpenDisplay(NULL);
 	if (!display) {
 		fprintf(stderr, "%s\n", "main: XOpenDisplayError");
+		free(png);
 		exit(EXIT_FAILURE);
 	}
 	int screen = XDefaultScreen(display);
 	fprintf(stdout, "main: using screen %d\n", screen);
 	XCloseDisplay(display);
+	free(png);
 	display = NULL;
+	png = NULL;
 	return 0;
 }
