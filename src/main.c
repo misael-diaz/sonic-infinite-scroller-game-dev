231d4b3f141b08e89f7005e93862d174e750067c
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-05-21 12:20:09 -0400

    opens display

--- src/main.c
+++ src/main.c
@@ -20,7 +20,20 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
+
 int main(void)
 {
+	Display *display = XOpenDisplay(NULL);
+	if (!display) {
+		fprintf(stderr, "%s\n", "main: XOpenDisplayError");
+		exit(EXIT_FAILURE);
+	}
+	int screen = XDefaultScreen(display);
+	fprintf(stdout, "main: using screen %d\n", screen);
+	XCloseDisplay(display);
+	display = NULL;
 	return 0;
 }
